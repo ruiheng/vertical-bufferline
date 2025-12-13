e16df8c7cef81325421f2c033ef6607fde642142
Gilbert Fine <gilbert.fine@gmail.com>
2025-12-13 15:18:09 +0800

    fix: disable 'statuscolumn' in VBL window

--- init.lua
+++ init.lua
@@ -2093,6 +2093,7 @@ local function open_sidebar()
     api.nvim_win_set_option(new_win_id, 'relativenumber', false)
     api.nvim_win_set_option(new_win_id, 'cursorline', false)
     api.nvim_win_set_option(new_win_id, 'cursorcolumn', false)
+    api.nvim_win_set_option(new_win_id, 'statuscolumn', '')  -- Disable statuscolumn to prevent column usage
     
     -- Ensure mouse support is enabled for this window
     if vim.o.mouse == '' then
